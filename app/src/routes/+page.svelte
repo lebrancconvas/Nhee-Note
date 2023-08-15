99c8f264a1e3db0e88b370ef533d55a77f6ead41
lebrancconvas <lebrancconvas@gmail.com>
2023-08-15 14:04:34 +0700

    ✨ feat: header and footer on landing page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // your script goes here
+</script>
+
+<svelte:head>
+  <title>Nhee Note | บันทึกหนี้</title>
+</svelte:head>
+
+<div>
+  <header>
+    <h1>Nhee Note</h1>
+  </header>
+  <main>
+
+  </main>
+  <footer>
+    <p>Copyright 2023, Made by <a href="https://github.com/lebrancconvas">Poom Yimyuean (@lebrancconvas)</a></p>
+  </footer>
+</div>
+
+<style>
+  /* your styles go here */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  header {
+    text-align: center;
+  }
+
+  footer {
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    background-color: white;
+  }
+</style>
+
+<!-- markup (zero or more items) goes here -->
